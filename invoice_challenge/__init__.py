1354c3cfa44746f3b3732c467da0702edbeaf0eb
RAFAEL_LOBO <rlobotech@gmail.com>
2020-07-16 00:57:13 -0300

    apply correct url naming for restful api

--- invoice_challenge/__init__.py
+++ invoice_challenge/__init__.py
@@ -7,5 +7,5 @@ api = Api(app, prefix="/api/v1")
 
 # It seems that flask_restful was not build to work with filters.
 # In order to work with filters I will add a resource specific for filters.
-api.add_resource(InvoiceCollection, '/invoice')
-api.add_resource(Invoice, '/invoice/<id>')
+api.add_resource(InvoiceCollection, '/invoices')
+api.add_resource(Invoice, '/invoices/<id>')
